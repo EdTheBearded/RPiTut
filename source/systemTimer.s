011ce1daa4fe51dd5f38ab1d78d8d9a9a6091e9e
labrax <labrax@gmail.com>
2014-06-01 21:28:32 +0100

    Fixed description on systemWait. Added a doubleword version of the busy wait (systemWait64). Added a folder with a description of the systemWait64 algorithm.

--- source/systemTimer.s
+++ source/systemTimer.s
@@ -13,13 +13,12 @@ GetTimeStamp:
 	ldrd	r0, r1, [r0, #4]
 	pop	{pc}
 
-@ wait the amount specified in r0 in miliseconds
+@ wait the amount specified in r0 in microseconds
 .globl SystemWait
 SystemWait:
 	endtime .req r2
 	mov 	endtime, r0
-	push	{lr}
-	add	sp, sp, #-4
+	stmfd sp!, {lr}
 	bl 	GetTimeStamp
 	add	endtime, endtime, r0
 	SystemWait_loop$:
@@ -27,5 +26,23 @@ SystemWait:
 		cmp	r0, endtime
 		blo	SystemWait_loop$
 	.unreq endtime
-	add 	sp, sp, #4
-	pop {pc}
+	ldmfd sp!, {pc}
+
+@ wait the amount specified in r1, r0 in microseconds
+.globl SystemWait64
+SystemWait64:
+	stmfd sp!, {r4, lr}
+	mov r4, r1
+	mov r3, r0
+	bl GetTimeStamp
+	add r3, r0, r3 @r4 and r4 will store the final time
+	add r4, r1, r4
+	SystemWait_loop64$:
+		bl GetTimeStamp
+		cmp r1, r4
+		bmi SystemWait_loop64$
+		bpl SystemWait_fim64$
+		cmp r0, r3
+		bmi SystemWait_loop64$
+	SystemWait_fim64$:
+	ldmfd sp!, {r4, pc}
